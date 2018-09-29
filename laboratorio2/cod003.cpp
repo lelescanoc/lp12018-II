c6546f314b4c0893be383d526f9816e2f05be13d
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-09-29 11:04:54 -0500

    cod003
    Area

---
+++ laboratorio2/cod003.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int length=20;
+	int width=40;
+	int area=length*width;
+	length=99;
+	
+		int perimeter=(length+width)*2;
+		int perimeter=length*2+width*2;
+		int perimeter=length+width/2;
+		cout<<perimeter;
+		return 0;
+}
