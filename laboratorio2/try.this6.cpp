9da8833d2bcf6ad2f9b044645d32831f62a7d2f6
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-09-29 16:07:54 -0500

    Update try.this6.cpp

--- laboratorio2/try.this6.cpp
+++ laboratorio2/try.this6.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+##include<iostream>
 #include<vector>
 #include<algorithm>
 #include<string.h>
@@ -12,24 +12,18 @@ int main()
 	no_gusta.push_back("shit");
 	no_gusta.push_back("mierda");
 	string pi="PIIII";
+	
 	while(cin>>temp)
 	{
 		words.push_back(temp);
-		cout<<" Numero de palabras: "<<words.size()<<endl;
-		sort(words.begin(),words.end());
-		{
-		for(int i=0;i<words.size();i++)
-		{
-			if(i==0 || words[i-1]!= words[i] && words[i]!=no_gusta[0] && words[i]!=no_gusta[1])
-			{
-			cout<<words[i]<<endl;
 		
+	for(int i=0;i<no_gusta.size();i++)
+		{
+			if(temp== no_gusta[i] )
+			{
+				temp="piiii";
 			}
-			else
-			cout<<pi<<endl;
-			
 		}
-		}
-
-	}	
+		cout<<temp<<" ";
+	}
 }
