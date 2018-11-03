02074ad314981253b38592676a77f05a764aa917
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-02 21:43:03 -0500

    code02

---
+++ Laboratorio3/Code02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int area(int length, int width);
+int main()
+{
+    int x0=area(7,8);
+    int x1=area(7,8);
+    int x2=area(7,2);
+    return area(4,4);
+}
+int area(int length, int width)
+{
+    return length*width;
+}
