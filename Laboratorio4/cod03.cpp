99a96cb161262bccc00dde59d7205dbf1b4d08f7
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-09 20:16:09 -0500

    cod03
    Declaracions y definiciones

---
+++ Laboratorio4/cod03.cpp
@@ -0,0 +1,36 @@
+int f(int);
+int main()
+{
+	int i=7;
+	cout<<f(i)<<'\n';	
+}
+double my_sqrt(double);
+extern int a;
+int a;
+int x=7;
+extern int x;
+extern int x;
+double my_sqrt(double);
+double my_sqrt(double d)
+{
+	return 0.0;
+}
+double my_sqrt(double);
+double my_sqrt(double);
+double expression();
+double primary()
+{
+	return expression();
+}
+double term()
+{
+	return primary();
+}
+double expression()
+{
+	return term();
+}
+int f(int n)
+{
+	return n;
+}
