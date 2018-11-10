dfc5ec094fb93fbead3f962181b4fdfbdbd84d6d
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-09 20:27:18 -0500

    Cod09
    Codigo malidentado

---
+++ Laboratorio4/Cod09.cpp
@@ -0,0 +1,40 @@
+struct X
+{
+	void f(int x)
+	{
+		struct Y
+		{
+			int f()
+			{
+				return 1;
+			}
+			int m;
+		};
+		int m;
+		m=x; Y m2;
+		return f(m2.f());
+		}
+		if (m) f(m+2);
+		else
+		{
+			g(m+2);
+		}
+		}
+		X()
+		{
+		}
+		void m3()
+		{
+		}
+		void main()
+		{
+			X a; a.f(2);
+		}
+	}
+};
+int main()
+{
+	g(0);
+	f();
+	f1(0.1); X x; x.main(); x.m3();
+}
