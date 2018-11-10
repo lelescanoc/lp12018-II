61c716a5e3965d7058dac60f496dc0ed7fd04ab3
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-10 09:26:43 -0500

    cod03
    Struct Clasepublica por Defecto

---
+++ Laboratorio5/cod03.cpp
@@ -0,0 +1,22 @@
+struct Date
+{
+	int y;
+	int m;
+	int d;
+};
+void init_day(Date& dd, int y, int m,int d)
+{
+	//verificar si es valida
+	//si lo es , inicializar
+}
+void add_day(Date& dd, int n)
+{
+	//incrementa dias n
+}
+int main()
+{
+	Date hoy;
+	init_day(hoy, 12,24,2005);
+	add_day(today ,1);
+	return 0;
+}
