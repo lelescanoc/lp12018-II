a3488b1ddf72338ac1db4f806e688e4b980bc5c9
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-29 20:22:40 -0500

    cod04
    contrusctor

---
+++ DiaposClases/cod04.cpp
@@ -0,0 +1,22 @@
+struct Date {
+	int y;
+	int m;
+	int d;
+	
+};
+void init_day(Date&dd,int y, int m, int d)
+{
+	//verificar si es valida
+	//si lo es inicializar la fecha
+}
+void add_day(Date&dd, int n)
+{
+	//incremetna n dias
+}
+int main ()
+{
+	Date hoy;
+	init_day(hoy,12,24,2005); //error
+	//dia 2005 del ano 12
+	add_ day(today,1);
+	
