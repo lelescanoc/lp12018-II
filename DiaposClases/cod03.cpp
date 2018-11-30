42a5d8d9864d5c13aaba0a26234a0bf2a6886b4f
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-29 20:22:03 -0500

    cod03
    funcionen clases

---
+++ DiaposClases/cod03.cpp
@@ -0,0 +1,26 @@
+struct Date{
+	int y; //year
+	int m;//month in year
+	int d;//day of month
+};
+//funciones de ayuda
+void init_day(Date& dd,int y,int m, int d)
+{
+	//verificar si es valida
+	//si lo es, inicialixar la fecha}
+	
+}
+void add_day(Date & dd, int n)
+{
+	//incrementa n dias
+}
+int main()
+{
+	Date hoy;
+	init_day(hoy,12,24,2005);//error
+	//dia 2005 del ano12
+	add_day(today,1);
+	return 0;
+		
+		
+}
