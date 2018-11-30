2a6b55a5338884291b63bdd6a9b1d3dd0d0160fe
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-30 09:01:10 -0500

    cod08
    Puntero Nulo

---
+++ Punteros/cod08.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double *p0=0;
+	double *p0=nullptr;
+	if (p0 !=0)
+	{
+		//si p0 es valido
+	}
+	if(p0)
+	{
+		//p0 es valido, igual que p0!=0
+	}
+}
