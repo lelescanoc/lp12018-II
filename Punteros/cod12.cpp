3374ceffe69ed145bcf65d24c7212df9e1b8d876
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-30 09:43:46 -0500

    cod12
    Punteros a void

---
+++ Punteros/cod12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	void *pv1=new int;
+	void *pv2=new double[10];
+}
+void f(void *pv)
+{
+	void *pv2=pv;
+	int *pi=static_cast<int*>(pv);
+}
+
