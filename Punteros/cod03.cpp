6ab967dda3ecfed1decb2b06d4ec07b1bf428e37
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-30 08:38:18 -0500

    cod03
    Asignacion de memoria libre

---
+++ Punteros/cod03.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n=7;
+	int *pi=new int;
+	int *qi=new int[4];
+	
+	double *pd=new double;
+	double *qd=new double[n];
+	
+	
+}
