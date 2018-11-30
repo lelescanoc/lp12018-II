9184bce64b80977faa2425aea81e4e1f84fb316f
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-30 08:59:07 -0500

    cod07
    Liberando memoria

---
+++ Punteros/cod07.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+double *calc(int res_size, int max)
+{
+	double *p=new double[max];
+	double *res=new double[res_size];
+	delete []p;
+	return res;
+}
+int main()
+{
+	double *r=calc(100,1000);
+	delete[]r;
+	
+}
