97eae30f5669ed1a80c3b253d41241e0a62b477c
lelescanoc <38143586+lelescanoc@users.noreply.github.com>
2018-11-30 08:40:51 -0500

    cod04
    Rangos

---
+++ Punteros/cod04.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	double *p=new double[4];
+	double x=*p;
+	double y=p[2];
+	
+	*p=7.7;
+	p[2]=9.9;
+	{
+		double x=p[3];
+		p[3]=4.4;
+		double y=p[0];
+	}
+}
